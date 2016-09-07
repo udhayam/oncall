d740acf2bbb45ffb07eee57489841ed9cdfa7b25
MrU <p.udhayam1113@gmail.com>
2016-09-08 02:45:11 +0530

    bower setup

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var gulp = require('gulp');
+
+
+/**
+ *  Default task clean temporaries directories and launch the
+ *  main optimization build task
+ */
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+});
